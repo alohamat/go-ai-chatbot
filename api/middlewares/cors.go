7c8a0e1905fad30e0ae20d26966e2a51f4f1374a
alohamat <matalohazk@gmail.com>
2025-10-08 10:03:34 -0300

    changing routes for deploy

--- api/middlewares/cors.go
+++ api/middlewares/cors.go
@@ -7,9 +7,9 @@ import (
 
 func CorsMiddleware(handler http.Handler) http.Handler {
 	return cors.New(cors.Options{
-		AllowedOrigins: []string{"*"},
+		AllowedOrigins: []string{"https://react-ai-frontend-gamma.vercel.app", "http://localhost:8080"},
 		AllowedMethods: []string{"POST", "OPTIONS"},
 		AllowedHeaders: []string{"Content-Type"},
-		AllowCredentials: false,
+		AllowCredentials: true,
 	}).Handler(handler)
 }
