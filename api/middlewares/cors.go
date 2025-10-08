08f56b252ecb90e9c17f6eb3295ae1af7ff445f4
aloha <matalohazk@gmail.com>
2025-10-08 18:06:47 -0300

    changing cors routes

--- api/middlewares/cors.go
+++ api/middlewares/cors.go
@@ -7,7 +7,7 @@ import (
 
 func CorsMiddleware(handler http.Handler) http.Handler {
 	return cors.New(cors.Options{
-		AllowedOrigins: []string{"https://react-ai-frontend-gamma.vercel.app", "http://localhost:8080"},
+		AllowedOrigins: []string{"https://react-ai-frontend-chi.vercel.app/", "http://localhost:8080"},
 		AllowedMethods: []string{"POST", "OPTIONS"},
 		AllowedHeaders: []string{"Content-Type"},
 		AllowCredentials: true,
