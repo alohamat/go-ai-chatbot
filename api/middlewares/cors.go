17cc0b575476f7eeb3eef1ca8d6753075661427f
aloha <matalohazk@gmail.com>
2025-10-08 18:11:39 -0300

    fixing cors error

--- api/middlewares/cors.go
+++ api/middlewares/cors.go
@@ -9,7 +9,7 @@ func CorsMiddleware(handler http.Handler) http.Handler {
 	return cors.New(cors.Options{
 		AllowedOrigins: []string{"https://react-ai-frontend-chi.vercel.app/", "http://localhost:8080"},
 		AllowedMethods: []string{"POST", "OPTIONS"},
-		AllowedHeaders: []string{"Content-Type"},
+		AllowedHeaders: []string{"Content-Type", "Authorization", "Access-Control-Allow-Origin"},
 		AllowCredentials: true,
 	}).Handler(handler)
 }
